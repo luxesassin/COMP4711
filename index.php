d5a7bedd45142f59989cb343a6bc5ff0b2e35571
Jamie <luxeser@gmail.com>
2017-01-12 14:57:40 -0800

    Added Student class in Student.php and changed 'index.php'

--- index.php
+++ index.php
@@ -1,12 +1,53 @@
 <!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Jamie Lee</title>
+        <title></title>
     </head>
     <body>
         <?php
-        echo "Hello out there ... I live";
+        include('Student.php');
+        $students = array();
+        $first = new Student();
+        $first->surname = "Doe";
+        $first->first_name = "John";
+        $first->add_email('home','john@doe.com');
+        $first->add_email('work','jdoe@mcdonalds.com');
+        $first->add_grade(65);
+        $first->add_grade(75);
+        $first->add_grade(55);
+        $students['j123'] = $first;
+        
+        $second = new Student();
+        $second->surname = "Einstein";
+        $second->first_name = "Albert";
+        $second->add_email('home','albert@braniacs.com');
+        $second->add_email('work1','a_einstein@bcit.ca');
+        $second->add_email('work2','albert@physics.mit.edu');
+        $second->add_grade(95);
+        $second->add_grade(80);
+        $second->add_grade(50);
+        $students['a456'] = $second;
+
+        $third = new Student();
+        $third->surname = "Lee";
+        $third->first_name = "Jamie";
+        $third->add_email('home','luxeser@gmail.com');
+        $third->add_email('work1','luxeser@gmail.com');
+        $third->add_email('work2','luxeser@gmail.com');
+        $third->add_grade(100);
+        $third->add_grade(100);
+        $third->add_grade(100);
+        $students['A00922798'] = $third;
+        
+        ksort($students); // one of the many sort functions
+        foreach($students as $student)
+            echo $student->toString();
         ?>
     </body>
 </html>
