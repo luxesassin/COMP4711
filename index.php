204623e9858b89efde40456f163ac6c791e60a99
Jamie <luxeser@gmail.com>
2017-01-12 14:45:38 -0800

    Setup

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Jamie Lee</title>
+    </head>
+    <body>
+        <?php
+        echo "Hello out there ... I live";
+        ?>
+    </body>
+</html>
